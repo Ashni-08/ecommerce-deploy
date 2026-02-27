c1911357c874662d6ef7e27be596aeea2cce41b6
Anushka-0009 <tkanushka08@gmail.com>
2026-02-27 13:55:02 +0530

    Clean nested repo and add full project

--- frontend/src/Routing.jsx
+++ frontend/src/Routing.jsx
@@ -5,45 +5,21 @@ import Navbar from './Navbar'
 import Home from './Home'
 import Login from './Login'
 import Register from './Register'
-import DynamicNavigation from './DynamicNavigation'
 import OrderPage from "./OrderPage";
-
-import YourOrders from './YoursOrders'
-
+import YourOrdersPage from './YourOrdersPage';
 import MyCart from "./MyCart";
-
-
 
 function Routing() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      
       <Route path="/product/:source/:id" element={<Product />} />
       <Route path="/order" element={<OrderPage />} />
       <Route path="/navbar" element={<Navbar />} />
-<Route path='/MyCart' element={<MyCart/>}/>
-<Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
-
-
-
-
-
-      
-
-     
-
-<Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
-
-
+      <Route path="/MyCart" element={<MyCart />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/your-orders" element={<YourOrders />} />
-
-
-
+      <Route path="/your-orders" element={<YourOrdersPage />} />
     </Routes>
   )
 }
